93fc41f3d4fe4d2b1a7b7fbd38581f19a645926c
BushraIrfan456 <bi6164989@gmail.com>
2024-09-11 13:15:23 +0500

    js code

---
+++ index.js
@@ -0,0 +1,91 @@
+//  single line
+
+//     Multi 
+//      Line
+
+     
+
+//   alert('External file')
+
+// 'Mai "string" hun'
+
+//  "mai bhi string 'abcd' hun koi shak "
+
+// // String: Set of Characters  enclosed in single or double quotes
+
+//  var x ; // Declaration
+//  console.log(x)  //undefine
+
+// //  = ----> Assignment operator
+
+//  var x =  "";
+//  console.log(x) ; /// empty
+
+//  var x =  "Variable x";
+//  console.log(x) ;
+//  console.log(x) ;
+//  console.log(x) ;
+//  console.log(x) ;
+//  console.log(x) ;
+
+//    var amount = "450" ;
+//  console.log(amount);
+//  console.log(amount);
+//  console.log(amount);
+// console.log(amount);
+
+//  var name = "Ruhama";
+//  alert(name);
+
+// var y = "Variable y";
+
+// var y; // Declaration
+//  y = "Variable y" // Initialization
+//  y = "I am Variable y"  // reassign
+//  console.log(y);  // I am Variable y
+
+//  y = "mai variable y hun"
+// console.log(y); // mai variable y hun
+
+//  email = "abcd@gmail.com";  // not good practice
+//  console.log(email); 
+
+// var value5 = "Value five"
+// console.log("Value one")
+// console.log("Value two");
+// console.log("value Three ," , "value four ," ,value5);
+// // ------------------------------------------
+
+
+
+// javascript class 5
+
+// &&
+ console.log(true && true);  
+ console.log(true && false);
+ console.log(true && false && true);
+ console.log("myString" && 'undefined' && undefined);
+
+//  ||
+console.log(true || false);  
+console.log('myString'|| undefined || true);
+console.log(null || true && 'myString'&& false);
+
+
+
+
+
+var percent = prompt('enter ypur percentage')
+
+if(percent <= 80){
+  alert('your grade is A1')
+
+}
+
+else if (percent <= 70){
+  alert('you are B')
+}
+
+else {
+  alert('you are fail')
+}
